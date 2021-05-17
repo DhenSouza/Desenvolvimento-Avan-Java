906ece11b013dbbc13cce07b436c3ca666a8b22f
DhenSouza <ds052798ts@gmail.com>
2021-05-17 16:27:11 -0300

    comentarios aplicados

--- src/digitalInnovation/FatorialRecursivo/ComposicaoDeFuncoes.java
+++ src/digitalInnovation/FatorialRecursivo/ComposicaoDeFuncoes.java
@@ -7,12 +7,13 @@ public class ComposicaoDeFuncoes {
     public static void main(String[] args) {
         int [] valores = {1,2,3,4};
 
-        /*Arrays.stream(valores)
+        // Funcional
+        Arrays.stream(valores)
                 .filter(numero -> numero % 2 == 0)
                 .map(numero -> numero * 2)
-                .forEach(numero -> System.out.println(numero));*/
+                .forEach(numero -> System.out.println(numero));
 
-
+        // Imperativo
          for (int i = 0; i < valores.length; i++) {
             int valor = 0;
             if(valores[i] % 2 == 0){
