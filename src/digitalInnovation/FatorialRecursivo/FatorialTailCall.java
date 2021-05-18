3c5d4d53cf6a494a678ef49e6b17b5dbd879f3f9
DhenSouza <ds052798ts@gmail.com>
2021-05-18 16:53:43 -0300

    exemplo de fatorial tailcall

---
+++ src/digitalInnovation/FatorialRecursivo/FatorialTailCall.java
@@ -0,0 +1,16 @@
+package digitalInnovation.FatorialRecursivo;
+
+public class FatorialTailCall {
+    public static void main(String[] args) {
+        System.out.println(fatorialA(5));
+    }
+    public static double fatorialA(double valor) {
+        return fatorialComTailCall(valor, 1);
+    }
+    private static double fatorialComTailCall(double valor, double numero) {
+        if(valor == 0){
+            return numero;
+        }
+        return fatorialComTailCall(valor-1, numero * valor);
+    }
+}
