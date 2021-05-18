99d5d2bebe39d6c9bcb0e4e4f95c98888054ac76
DhenSouza <ds052798ts@gmail.com>
2021-05-18 16:30:50 -0300

    Aplicando exemplo de Fatorial Recursivo

--- src/digitalInnovation/FatorialRecursivo/FatorialRecursivo.java
+++ src/digitalInnovation/FatorialRecursivo/FatorialRecursivo.java
@@ -1,4 +1,19 @@
 package digitalInnovation.FatorialRecursivo;
 
 public class FatorialRecursivo {
+
+    public static void main(String[] args) {
+        System.out.println("Resultado é: " + fatorial(5));
+    }
+
+    public static int fatorial (int value ) {
+        if(value == 1) {
+            return value;
+        } else {
+            return value * fatorial((value -1));
+        }
+    }
+    /* fatorial de: 5
+    * 55 * 7 * 3 * 2 * 1
+    * resultado: 120 */
 }
