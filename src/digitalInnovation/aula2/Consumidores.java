4b1342a057afbfb86a22d9b80c7f74bea379c232
DhenSouza <ds052798ts@gmail.com>
2021-05-20 16:42:46 -0300

    Exemplo de Consumer

---
+++ src/digitalInnovation/aula2/Consumidores.java
@@ -0,0 +1,15 @@
+package digitalInnovation.aula2;
+
+import java.util.function.Consumer;
+
+public class Consumidores {
+    public static void main(String[] args) {
+        // Method Reference
+//      -Apenas
+//      - Utilizar o parametro de forma que ele foi recebido
+
+        Consumer<String> imprimirUmaFrase = System.out::println;
+        Consumer<String> imprimirUmaFrase2 = frase -> System.out.println(frase);
+        imprimirUmaFrase.accept("Hello Word");
+    }
+}
